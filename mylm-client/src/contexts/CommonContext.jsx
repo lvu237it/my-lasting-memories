9ac1985fdcf56042e581dbf086be7f6f8cd68a43
lvu237it <luuvanvua7k16vt@gmail.com>
2024-08-04 00:17:46 +0700

    Deploying

--- mylm-client/src/contexts/CommonContext.jsx
+++ mylm-client/src/contexts/CommonContext.jsx
@@ -162,8 +162,8 @@ export const Common = ({ children }) => {
   const navigate = useNavigate();
   const { from } = location.state || { from: '/' }; // Nếu không có thông tin from thì mặc định về trang chủ
 
-  const apiBaseUrl = import.meta.env.VITE_API_BASE_URL_DEVELOPMENT;
-  // const apiBaseUrl = import.meta.env.VITE_API_BASE_URL_PRODUCTION;
+  // const apiBaseUrl = import.meta.env.VITE_API_BASE_URL_DEVELOPMENT;
+  const apiBaseUrl = import.meta.env.VITE_API_BASE_URL_PRODUCTION;
 
   useEffect(() => {
     if (viewPostDetails) {
